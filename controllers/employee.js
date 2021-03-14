571e37f97fac4b4115fba809ab62c6b6fac2a59f
i'mfebri <iamfebriadji@gmail.com>
2021-03-14 16:19:51 +0700

    Employee: Add the put method

--- controllers/employee.js
+++ controllers/employee.js
@@ -3,7 +3,7 @@ const
   conn = require('../config/db_conn')
 
 router.get('/', async (req, res, next) => {
-  return await conn.query('select * from employee', (err, result) => {
+  return await conn.query('SELECT * FROM employee', (err, result) => {
     if (err) return new Error(err)
 
     res.status(200).json(result)
@@ -20,7 +20,7 @@ router.post('/', async (req, res, next) => {
   }
 
   const insertEmployee = () => new Promise((resolve, reject) => {
-    const sql = `insert into employee values (
+    const sql = `INSERT INTO employee VALUES (
       0, '${first_name}', '${last_name}', '${position}'
     )`
 
@@ -36,8 +36,8 @@ router.post('/', async (req, res, next) => {
   insertEmployee().then(result => {
     // enter data into child tables
     const insertDetails = () => new Promise((resolve, reject) => {
-      const sql = `insert into employee_details (nik_employee, full_name, join_date)
-        values (${result.insertId}, '${fullname}', null)
+      const sql = `INSERT INTO employee_details (nik_employee, full_name, join_date)
+        VALUES (${result.insertId}, '${fullname}', null)
       `
 
       conn.query(sql, err => {
@@ -57,10 +57,48 @@ router.post('/', async (req, res, next) => {
   .catch(err => new Error(err))
 })
 
+router.put('/:nik', async (req, res, next) => {
+  const params = req.params.nik
+  const { first_name, last_name, position, age, gender, residence, salary } = req.body
+
+  // update all data from the two primary tables and child tables
+  const updateEmployee = () => new Promise((resolve, reject) => {
+    const sql = `UPDATE employee
+      SET first_name = ?, last_name = ?, position = ?
+      WHERE nik = ${params}
+    `
+
+    return conn.query(sql, [first_name, last_name, position], err => {
+      if (err) return reject(err)
+      resolve('successfully updated employee data')
+    })
+  })
+
+  const updateDetails = () => new Promise((resolve, reject) => {
+    const sql = `UPDATE employee_details
+    SET full_name = ?, age = ?, gender = ?, residence = ?, salary = ?
+    WHERE nik_employee = ${params}
+    `
+    // create a full name by combining the first name with the last name
+    const fullname = first_name.concat(' ' + last_name)
+
+    return conn.query(sql, [fullname, age, gender, residence, salary], err => {
+      if (err) return reject(err)
+      resolve('successfully updated employee data')
+    })
+  })
+
+  return await Promise.all([ updateEmployee(), updateDetails() ])
+    .then(result => res.status(200).json({
+      message: result[0]
+    }))
+    .catch(err => new Error(err))
+})
+
 router.delete('/:nik', async (req, res, next) => {
   try {
     const params = req.params.nik
-    const sql = await `delete from employee where nik = ?`
+    const sql = await `DELETE FROM employee WHERE nik = ?`
     
     return await conn.query(sql, [params], (err, result) => {
       if (err) return new Error(err)
